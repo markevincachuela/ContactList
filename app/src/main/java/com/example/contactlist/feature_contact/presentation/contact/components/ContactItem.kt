365745e8d2fdda8acebfa0ae5c24312e5928b800
Mark kevin cachuela <mark.cachuela@magneds.com>
2024-04-16 00:59:53 +0800

    Fix Add Edit delete behavior and create a dedicated textField

--- app/src/main/java/com/example/contactlist/feature_contact/presentation/contact/components/ContactItem.kt
+++ app/src/main/java/com/example/contactlist/feature_contact/presentation/contact/components/ContactItem.kt
@@ -6,9 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -22,7 +20,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import com.example.contactlist.R
 import com.example.contactlist.feature_contact.domain.model.Contact
 
 @Composable
@@ -80,7 +77,4 @@ fun ContactItem(
         }
 
     }
-
-
-
 }
